602c80feb49f8fee88a879690e6a52fa088d2a54
Rui <Rui914@users.noreply.github.com>
2015-05-09 05:36:02 +0900

    Update creeper.lua

--- creeper.lua
+++ creeper.lua
@@ -1,3 +1,12 @@
+local id = 1
+while true do
+	local file = io.open(minetest.get_modpath("mobtalker").."/creeper/"..id..".script")
+	if (not file) then break end
+	file:close()
+	dofile(minetest.get_modpath("mobtalker").."/creeper/"..id..".script")
+	id = id +1
+end
+
 -- intllib
 local S
 if minetest.get_modpath("intllib") then
@@ -6,63 +15,42 @@ else
 	S = function(s) return s end
 end
 
--- Hud Number
-local hudnumber
-if minetest.setting_getbool("enable_damage") then
-	hudnumber = 1
-else
-	hudnumber = 0
-end
-
--- Save
 --[[
-local function savelove()
+-- Save
+local function save()
 	local output = io.open(minetest.get_worldpath() .. "/mobtalker", "w")
 	for _,p in ipairs(minetest.get_connected_players()) do
-		output:write(creeper_love[p:get_player_name()].."\n")
+		output:write(creeper_love[p:get_player_name()]..p:get_player_name().."\n")
 	end
 	io.close(output)
 end
 
 -- Load
-local function loadlove()
-	local input = io.open(minetest.get_worldpath() .. "/mobtalker", "r")
+local function load()
+	local input = io.open(minetest.get_worldpath().."/mobtalker", "r")
 	if input then
 		repeat
 			local love = input:read("*n")
+			if love == nil then
+				break
+			end
 			local name = input:read("*l")
 			creeper_love[name:sub(2)] = love
-		until input:read() == nil
+		until input:read(0) == nil
 		io.close(input)
+		load()
 	else
 		creeper_love = {}
 	end
 end
 ]]--
-
--- Function
-function mobtalker_creeper(self,clicker)
-	--loadlove()
-	self.object:setvelocity({x=0,y=0,z=0})
-	creeper_talk[self] = true
-	creeper_talking[clicker:get_player_name()] = true
-	local hud1 = clicker:hud_add({
-		hud_elem_type = "image",
-		position = {x=0.55, y=0.6},
-		offset = {x=-30, y=0},
-		scale = {x=0.5, y=0.5},
-		text = "test.png",
-	});
-	minetest.show_formspec(clicker:get_player_name(),"creeper:talk",mobtalker_creeper_talk(clicker,creeper_love[clicker:get_player_name()],1))
-end
-
 -- Return Formspec
 function mobtalker_creeper_talk(player,love,Q)
 	if love == 0 and Q == 1 then
-		return "size[5,2;true]"..
-			"label[0,0;"..S("Hey! Who are you?").."]"..
-			"button_exit[0,0.6;4.8,0.5;creeper_choose1;"..S("My name is %s."):format(player:get_player_name()).."]"..
-			"button_exit[0,1.4;4.8,0.5;creeper_choose2;"..S("Your death!").."]"
+		return "size[5,3;true]"..
+			"label[0,0;"..SET_NAME.."\n"..S("Hey! Who are you?").."]"..
+			"button_exit[0,1.2;4.8,0.5;creeper_choose1;"..S("My name is %s."):format(player:get_player_name()).."]"..
+			"button_exit[0,2;4.8,0.5;creeper_choose2;"..S("Your death!").."]"
 	elseif love > 0 and Q == 1 then
 		return "size[5,2;true]"..
 			"label[0,0;"..S("Hmm?").."]"..
@@ -82,6 +70,33 @@ function mobtalker_creeper_talk(player,love,Q)
 	end
 end
 
+function mobtalker_creeper_image(love,Q)
+	if love == 0 and Q == 1 then
+		return "creeper_normal.png"
+	elseif love > 0 and Q == 1 then
+		return "creeper_happy.png"
+	elseif love < 0 and Q == 1 then
+		return "creeper_angry.png"
+	else
+		return "creeper_normal.png"
+	end
+end
+
+-- Function
+function mobtalker_creeper(self,clicker)
+	self.object:setvelocity({x=0,y=0,z=0})
+	creeper_talk[self] = true
+	creeper_talking[clicker:get_player_name()] = true
+	minetest.show_formspec(clicker:get_player_name(),"creeper:talk",mobtalker_creeper_talk(clicker,creeper_love[clicker:get_player_name()],1))
+	creeper_hud = clicker:hud_add({
+		hud_elem_type = "image",
+		position = {x=0.52, y=0.6},
+		offset = {x=-30, y=0},
+		scale = {x=0.8, y=0.8},
+		text = mobtalker_creeper_image(creeper_love[clicker:get_player_name()],1),
+	});
+end
+
 -- Event
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if fields.creeper_choose1 then
@@ -96,24 +111,25 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		creeper_talk = {}
 		creeper_love[player:get_player_name()] = creeper_love[player:get_player_name()] + 1
 		creeper_talking[player:get_player_name()] = false
-		player:hud_remove(hudnumber)
-		--savelove()
+		player:hud_remove(creeper_hud)
+		--save()
 	elseif fields.creeper_boom then
 		creeper_talk = {}
 		creeper_love[player:get_player_name()] = creeper_love[player:get_player_name()] - 1
 		creeper_talking[player:get_player_name()] = false
-		player:hud_remove(hudnumber)
+		player:hud_remove(creeper_hud)
 		minetest.after(0.2, function()
 			creeper_boom(player:getpos())
 		end)
 	elseif fields["quit"] == "true" then
 		creeper_talk = {}
 		creeper_talking[player:get_player_name()] = false
-		player:hud_remove(hudnumber)
+		player:hud_remove(creeper_hud)
 	end
 end)
 
 -- Auto Load
 minetest.register_on_joinplayer(function(player)
 	creeper_love[player:get_player_name()] = 0
+	--load()
 end)
