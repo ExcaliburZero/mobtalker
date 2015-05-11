17b9999102e9fd35182d15eed3002577d8b00d59
Rui <Rui914@users.noreply.github.com>
2015-05-11 15:46:40 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,21 @@
 mobtalker = {}
 mobtalker.datadir = minetest.get_worldpath().."/mobtalker"
+
+-- Run API
 dofile(minetest.get_modpath("mobtalker").."/api.lua")
+
+-- Run item registration
 dofile(minetest.get_modpath("mobtalker").."/item.lua")
+
+-- If the creeper mod is intalled, then run the creeper file
 if minetest.get_modpath("creeper") then
-	dofile(minetest.get_modpath("mobtalker").."/creeper.lua")
+    dofile(minetest.get_modpath("mobtalker").."/creeper.lua")
 end
+-- If the simple mobs mod is installed, then run the dungeon master file
+-- (beta)
 if minetest.get_modpath("mobs") then
-	--dofile(minetest.get_modpath("mobtalker").."/dm.lua")
+    --dofile(minetest.get_modpath("mobtalker").."/dm.lua")
 end
+
+-- Make a directory for modtalker, or read it if it already exists
 mobtalker_mkdir()
